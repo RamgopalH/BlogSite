3adeac1c7cdf06d6f4889e11472f835c673af8d5
RamgopalH <rg2792002@gmail.com>
2022-09-11 20:41:35 +0530

    Complete V1 of Blog S Site

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const { render } = require('ejs');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-var posts = [];
+var posts = [{
+    title: 'Default Post',
+    body: 'Just a ranod0m default post'
+},];
 
 const homeStartContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
@@ -39,6 +43,19 @@ app.post('/compose', (req, res)=> {
     res.redirect('./');
 });
 
+app.get('/post/:postTitle', (req, res)=> {
+    if(post = contains(posts, req.params.postTitle)) {
+        res.render('post', { post: post });
+    }
+});
+
 app.listen(3000,()=> {
     console.log('localhost:3000');
 });
+
+function contains(posts, title) {
+    for (let post of posts) {
+        if(_.lowerCase(post.title) === _.lowerCase(title)) return post;
+    }
+    return null;
+}
