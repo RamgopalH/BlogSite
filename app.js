503a0bd362da248b7a5515a0bc5cb91d32b9df9b
RamgopalH <rg2792002@gmail.com>
2022-09-14 21:46:07 +0530

    Add MongoDB Functionality

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 const _ = require('lodash');
 const { render } = require('ejs');
 const app = express();
+const mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost:27017/BlogDB').then(()=>{
+    console.log('Connected to Database');
+});
 
-var posts = [{
-    title: 'Default Post',
-    body: 'Just a ranod0m default post'
-},];
 
 const homeStartContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
@@ -18,8 +18,34 @@ const contactContent = 'Aenean felis dolor, rutrum sed lorem eu, imperdiet moles
 
 const aboutContent = 'Duis varius mattis leo, non dictum lectus gravida eu. Ut eget suscipit lorem. Praesent vulputate erat non posuere dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
 
+const PostSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    lowerCase: String,
+    postedOn: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
 app.get('/', (req, res)=> {
-    res.render('home', { homeStartContent: homeStartContent, posts: posts });
+    Post.find({}, (err, posts)=> {
+        if(posts.length===0) {
+            newPost = new Post({
+                title: 'Default Post',
+                content: 'Just a Random Default Post',
+                lowerCase: _.lowerCase('Default Post')
+            });
+            newPost.save().then((post)=> {
+                console.log(post);
+                res.redirect('/');
+            });
+        } else {
+            res.render('home', { homeStartContent: homeStartContent, posts: posts });
+        }
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -35,27 +61,23 @@ app.get('/compose', (req, res)=> {
 });
 
 app.post('/compose', (req, res)=> {
-    const post = {
+    const post = new Post({
         title: req.body.postTitle,
-        body: req.body.postBody
-    }
-    posts.push(post);
-    res.redirect('./');
+        content: req.body.postContent,
+    });
+    post.save().then((post)=>{
+        console.log(post);
+        res.redirect('/');
+    });
 });
 
-app.get('/post/:postTitle', (req, res)=> {
-    if(post = contains(posts, req.params.postTitle)) {
-        res.render('post', { post: post });
-    }
+app.get('/post/:postId', (req, res)=> {
+    Post.findOne({_id: req.params.postId}, (err, post)=> {
+        res.render('post', {post: post});
+    });
 });
 
 app.listen(3000,()=> {
     console.log('localhost:3000');
 });
 
-function contains(posts, title) {
-    for (let post of posts) {
-        if(_.lowerCase(post.title) === _.lowerCase(title)) return post;
-    }
-    return null;
-}
